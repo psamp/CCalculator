28ac7b659588f5d9c84f8476b475b2eb92aeeb1d
Princess Sampson <ps@kingonomics.com>
2016-08-08 13:41:45 -0400

    Initial commit

---
+++ CCalculator/BasicMath.h
@@ -0,0 +1,14 @@
+//
+//  BasicMath.h
+//  CCalculator
+//
+//  Created by Princess Sampson on 8/8/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#ifndef BasicMath_h
+#define BasicMath_h
+
+#include <stdio.h>
+
+#endif /* BasicMath_h */
