2f2a0c3f06ce855487711805761d92159feb325b
Princess Sampson <ps@kingonomics.com>
2016-08-08 17:11:59 -0400

    Got all base requirements working

--- CCalculator/main.c
+++ CCalculator/main.c
@@ -8,42 +8,94 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include "BasicMath.h"
 
-bool isInUserOptions(int number) {
+void generateNOddNumbers(int n) {
     
-    int options[4] = {1, 2, 3, 4};
-    
-    bool rtn = false;
-    
-    for (int i = 0; i < 3 ; i++) {
-        
-        if (&number == &options[i]) {
-            rtn = true;
+    for (int i = 0; i <= n; i++) {
+        if (i % 2 != 0 ) {
+            printf("%d \n", i);
         }
     }
-    
-    printf("inside isInUserOptions: %d", number);
-    
-    return rtn;
-    
+}
+
+
+double add(int a, int b) {
+    return a + b;
+}
+
+double subtract(int a, int b) {
+    return a - b;
+}
+
+double multiply(int a, int b) {
+    return a * b;
 }
 
 int main(int argc, const char * argv[]) {
     
-    printf("Would you like to do addition (enter 1), subtraction (enter 2), multiplication (enter 3), or divison(enter 4)\n?");
-    
-    int option = -1;
-    int _error = -1;
     bool running = true;
-    
-    _error = scanf("%d",&option);
+    char symbols[] = {'+', '-', '*', '/'};
+    char option[1];
     
     while (running) {
         
-        if (_error != 0 || isInUserOptions(option) == false) {
+        fpurge(stdin);
+        
+        printf(" Addition: enter + \n Subtraction: enter - \n Multiplication: enter * \n First n odd numbers: n \n Exit: enter x \n");
+        scanf("%c", option);
+        
+        double firstNum;
+        double secondNum;
+        
+        
+        if (option[0] == symbols[0]) {
+            
+            printf("Enter the first operand:");
+            scanf("%lf", &firstNum);
+            
+            printf("Enter the second operand:");
+            scanf("%lf", &secondNum);
+            
+            printf("\n%lf + %lf = %lf \n",firstNum, secondNum, add(firstNum, secondNum));
+            
+        } else if(option[0] == symbols[1]) {
+            
+            printf("Enter the first operand:");
+            scanf("%lf", &firstNum);
+            
+            printf("Enter the second operand:");
+            scanf("%lf", &secondNum);
+            
+            printf("\n%lf - %lf = %lf \n",firstNum, secondNum, subtract(firstNum, secondNum));
+            
+            
+        } else if(option[0] == symbols[2]) {
+            
+            printf("Enter the first operand:");
+            scanf("%lf", &firstNum);
+            
+            printf("Enter the second operand:");
+            scanf("%lf", &secondNum);
+            
+            printf("\n%lf * %lf = %lf \n",firstNum, secondNum, multiply(firstNum, secondNum));
+            
+            
+        } else if(option[0] == 'n') {
+            
+            printf("Enter how many odd numbers you want:");
+            
+            scanf("%lf", &firstNum);
+            
+            generateNOddNumbers(firstNum);
+            
+            
+        }
+        
+        else if(option[0] == 'x') {
+            
             running = false;
         }
+        
     }
     
     printf("Thanks for playing.\n");
